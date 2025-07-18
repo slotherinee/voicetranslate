c4429634a202d681c1a3e78540b3367a3f382c34
Pavel Tarasov <paveltarasov121@gmail.com>
2025-07-18 22:37:14 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const app = createApp(App)
 
 app.use(router)
 
 app.mount('#app')
+serviceWorkerRegistration.register(); 
