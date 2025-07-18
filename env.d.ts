475a21994655bb6e03ee185ab17dfcb8f010c4df
Pavel Tarasov <paveltarasov121@gmail.com>
2025-07-18 22:54:46 +0300

    test

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,2 @@
 /// <reference types="vite/client" />
+/// <reference path="./serviceWorkerRegistration.ts" />
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
+
+
 import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration.ts';
 
 const app = createApp(App)
 
 app.use(router)
 
 app.mount('#app')
-serviceWorkerRegistration.register(); 
+serviceWorkerRegistration.register(undefined); 
